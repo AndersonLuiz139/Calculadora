9a2eb8a5d814cd155bcd0b16d1ff59d6ff525981
Anderson Luiz <namechina09@gmail.com>
2026-03-26 14:58:41 -0300

    feat: implementação de operações, calculo e controle do display

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,24 @@
 const display = document.querySelector('.display');
 const buttons = document.querySelectorAll('button');
 
+const validButtons = ['+', '-', '*', '/', '(', ')', '.'];
+
 buttons.forEach((button) => {
   button.addEventListener('click', () => {
     const value = button.textContent;
 
-    if (!isNaN(value) || value === '.') display.value += value;
+    if (!isNaN(value) || validButtons.includes(value)) display.value += value;
+  
+    if (value === 'C') display.value = '';
+    
+    if (value === 'DEL') display.value = display.value.slice(0, -1);
+    
+    if (value === '=') {
+      try {
+        display.value = eval(display.value);
+      } catch {
+        display.value = 'Erro';
+      }
+    }
   });
 });
