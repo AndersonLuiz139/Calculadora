d1630202af2ce2c499bcd561f297896556754868
Anderson Luiz <namechina09@gmail.com>
2026-03-26 13:22:41 -0300

    feat: adiciona insercao de numeros no display

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+const display = document.querySelector('.display');
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const value = button.textContent;
+
+    if (!isNaN(value) || value === '.') display.value += value;
+  });
+});
